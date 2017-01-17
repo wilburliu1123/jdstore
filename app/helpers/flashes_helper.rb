497d862bd3bb009624ec44311b823e13275ba560
wilburliu <wilburliu1123@gmail.com>
2017-01-16 23:14:35 -0500

    add flashhelper and admin product controller

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,4 +1,4 @@
-module FLashesHelper
+module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
